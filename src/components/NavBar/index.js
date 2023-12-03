28d461f2b56186eebfcad44c717643f8ede5237d
Naveen123P <125892820+Naveen123P@users.noreply.github.com>
2023-12-03 17:01:13 +0530

    Update index.js

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,1 +1,25 @@
 // Write your code here.
+import './index.css'
+
+const NavBar = props => {
+  const {score, topScore} = props
+
+  return (
+    <div className="navbar-container">
+      <div className="emoji-logo-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+          alt="emoji logo"
+          className="emoji-logo-img"
+        />
+        <p className="emoji-game">Emoji Game</p>
+      </div>
+      <div className="scores-container">
+        <p className="">Score: {score}</p>
+        <p className="">Top Score: {topScore}</p>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
